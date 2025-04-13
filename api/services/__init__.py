04842adef87ccfd5cf96d5f1dd959ebd2a98f2a3
tombe <mail@thomas-ruff.de>
2025-04-13 18:13:09 +0200

    Work in Progress - Models changed

--- api/services/__init__.py
+++ api/services/__init__.py
@@ -5,11 +5,13 @@ from .appointment_service import AppointmentService
 from .location_service import LocationService
 from .person_service import PersonService
 from .plan_service import PlanService
+from .auth_service import AuthService
 
 __all__ = [
     'CalendarService',
     'AppointmentService',
     'LocationService', 
     'PersonService', 
-    'PlanService'
+    'PlanService',
+    'AuthService'
 ]

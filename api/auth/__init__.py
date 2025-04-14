327d191ba3a90a14484266c11eb5ce61ed1035dc
tombe <mail@thomas-ruff.de>
2025-04-14 02:13:50 +0200

    Work in Progress - OAuth2

--- api/auth/__init__.py
+++ api/auth/__init__.py
@@ -7,12 +7,12 @@ from .oauth2 import (
 )
 from .dependencies import (
     RoleChecker, require_employee, require_dispatcher,
-    require_admin, require_superviser, allow_guest
+    require_admin, require_supervisor, allow_guest
 )
 from .cookie_auth import (
     get_token_from_cookie, get_current_user_from_cookie,
     WebRoleChecker, require_web_guest, require_web_employee, require_web_dispatcher,
-    require_web_admin, require_web_superviser
+    require_web_admin, require_web_supervisor
 )
 
 __all__ = [
@@ -22,8 +22,8 @@ __all__ = [
     'get_current_user', 'get_current_active_user',
     'get_password_hash', 'verify_password', 'get_user',
     'RoleChecker', 'require_employee', 'require_dispatcher',
-    'require_admin', 'require_superviser', 'allow_guest',
+    'require_admin', 'require_supervisor', 'allow_guest',
     'get_token_from_cookie', 'get_current_user_from_cookie',
     'WebRoleChecker', 'require_web_guest', 'require_web_employee', 'require_web_dispatcher',
-    'require_web_admin', 'require_web_superviser'
+    'require_web_admin', 'require_web_supervisor'
 ]
